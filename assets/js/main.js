b1b3f45ff44fc24f412ecc95f172f9925a477331
candramonika <candramonika946@gmail.com>
2026-08-13 22:36:23 +0800

    Update portofolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,3 @@
-/*=============== EMAIL JS ===============*/
-
-
 /*=============== ACHIEVEMENTS ===============*/ 
 function openCertificate(img) {
   const modal = document.getElementById("certificateModal");
@@ -35,9 +32,70 @@ function closeProject() {
 }
 
 
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+  e.preventDefault()
+
+  //serviceID - templateID - #form - publicKey
+  emailjs.sendForm('service_md3g7qb','template_091hb0w','#contact-form','-sab0urCMyXrnDfky')
+
+  .then(() =>{
+    contactMessage.textContent = 'Message sent successfully ✅'
+
+    setTimeout(() =>{
+      contactMessage.textContent = ''
+    }, 5000)
+
+    contactForm.reset()
+  })
+}
+contactForm.addEventListener('submit', sendEmail)
+
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+  const scrollUp = document.getElementById('scroll-up')
+  this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+  const scrollDown = window.scrollY
+
+  sections.forEach(current =>{
+    const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionsClass = document.querySelector('.nav__list a[href*=' + sectionId + ']')
+
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+      sectionsClass.classList.add('active-link')
+    }else{
+      sectionsClass.classList.remove('active-link')
+    }
+  })
+}
+
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: 'top',
+  distance: '60px',
+  duration: 2500,
+  delay: 400,
+})
 
+sr.reveal('.perfil, .contact__form')
+sr.reveal('.info', {origin: 'left', delay: 800})
+sr.reveal('.skills', {origin: 'left', delay: 1000})
+sr.reveal('.projects', {interval: 100})
